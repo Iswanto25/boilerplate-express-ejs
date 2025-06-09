8ded3b5c5b421168ce6ed5fc8c8851ea6ba3dd5e
Iswanto <uncle.dev03@outlook.com>
2025-06-09 11:06:56 +0700

    update: strukter vies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
- 
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+	res.render("index", { title: "Hello from TypeScript + EJS!" });
+});
+
+app.get("/send", (req, res) => {
+	res.send("Hello from the /send route!");
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on http://localhost:${port}`);
+});
